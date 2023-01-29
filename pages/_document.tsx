b18450879ef8f4bd92ea5006612f3a57537e5a0c
TingNg <xnxgxsx@hotmail.com>
2023-01-29 11:46:32 +1100

    Init netlify cms

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+          <script defer src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
+        </Head>
       <body>
         <Main />
         <NextScript />
